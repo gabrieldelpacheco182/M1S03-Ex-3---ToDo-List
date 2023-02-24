7c55879d21f84823c9712675d8be65bad651569d
gabrieldelpacheco182 <gabriel_o_pacheco@estudante.sesisenai.org.br>
2023-02-24 16:58:33 -0300

    arrumado function buscar, criado function alterar, tirardo os comentarios do arquivo html e js.

--- function.js
+++ function.js
@@ -1,55 +1,61 @@
+
 function buscarHobbies() {
+    
     let hobbiSearch = prompt("Digite um hobbi.")
-listaHobbies.forEach((hobbi) => {
-if(hobbi=='hobbi') {
-return alert(`Hobbies existente ${hobbi}`)
-} else {
-    alert("Hobbie não encontrado")
-    let addItem = prompt("Digite (SIM) para adicionar um novo hobbie.")
-    if(addItem=='sim'){
-        addHobbies()
-    }else {
-        operacao()
-    }
-}
-})
-}
-function addHobbies() {
-    listaHobbies.push(prompt("O que quer adicionar?"));
-    let novoAdd = prompt("Digite (NAO) para não adicionar mais hobbie.")
-    if(novoAdd =='nao') {
+    for (let i = 0; i < listaHobbies.length; i++) {
+     const buscar = listaHobbies.find((item) => item === hobbiSearch);/* find função para pesquisar */
+
+      if (buscar == hobbiSearch) {
+        alert(`Item existente ${buscar}`);
+        operacao();
+      } else {
+        alert("Item não existe");
         operacao();
     }
-}
+       } 
+console.log(listaHobbies);
+      }
 
+function addHobbies() {
+    listaHobbies.push(prompt("O que quer adicionar?"));  
+    alert(`Sua nova lista ${listaHobbies}`);
+    console.log(listaHobbies);
+    operacao();
+}
 function removeHobbies() {
     let removerFinal = prompt("Deseja mesmo remover o ultimo item da lista? Digite (sim)")
     if(removerFinal=='sim') {
     let hobbiespop = listaHobbies.pop();
-    operacao();
-    }else {
-        operacao();
-    }
+    alert(`O item ${hobbiespop} foi excluido`);
+}
+console.log(listaHobbies);
+operacao();
 }
 
 function alterHobbies() {
-    
+    const antigo = prompt("Selecione o elemento que quer alterar");
+        const novo = prompt("Ecreveva um novo elemento");
+
+        const index = listaHobbies.indexOf(antigo);
+
+        if (index !== -1) {
+          listaHobbies[index] = novo;
+        }
+        console.log(listaHobbies);
+    operacao();
 }
 function mostrarHobbies(){
     alert(`Lista de hobbies exsitentes ${listaHobbies}`);
-    let voltarMenu = prompt("Digite (sim) para voltar ao menu principal")
-    if(voltarMenu =='sim'){
-        operacao();
-    }
+    console.log(listaHobbies);
+    operacao();
 }
  var listaHobbies = ["correr", "andar"]
- console.log(listaHobbies);
-
-function operacao () {
-let operacao = parseInt(prompt("1 - buscar um hobbies\n 2 - Adicionar um hobbies \n 3 - Remover um hobbies \n 4 - Alterar um hobbies \n 5 - Lista de hobbies"));
- switch (operacao) {
-    case 1:
-        buscarHobbies();
+ 
+ function operacao () {
+     let operacao = parseInt(prompt("1 - buscar um hobbies\n 2 - Adicionar um hobbies \n 3 - Remover um hobbies \n 4 - Alterar um hobbies \n 5 - Lista de hobbies"));
+     switch (operacao) {
+         case 1:
+             buscarHobbies();
         break;
         case 2:
         addHobbies();
@@ -63,9 +69,10 @@ let operacao = parseInt(prompt("1 - buscar um hobbies\n 2 - Adicionar um hobbies
         case 5:
             mostrarHobbies();
             break;
-    default:
+    default:    alert("Errou.")
         break;
  }
 }
+console.log(listaHobbies);
 
  operacao();
